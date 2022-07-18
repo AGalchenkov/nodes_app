751013bbf589e97955acbe70e592406aeaa62720
Andrey Galchenkov <galchenkov@bk.ru>
2022-07-18 09:55:50 +0300

    escape commit :)

--- avl_check.py
+++ avl_check.py
@@ -1,21 +1,32 @@
 from nodes.models import *
 import os
 
-u = Units.objects.filter(mng_ip__isnull=False)
-for item in u:
-    resp = os.system(f'ping -c 1 -w 1 {item.mng_ip}')
-    if not resp:
-        item.is_avaliable = True
-        item.save()
+def main():
+    u = Units.objects.filter(mng_ip__isnull=False)
+    for item in u:
+        resp = os.system(f'ping -c 1 -w 1 {item.mng_ip}')
+        if not resp:
+            item.is_avaliable = True
+            item.save()
+        else:
+            item.is_avaliable = False
+            item.save()
+    u = Units.objects.filter(ipmi_bmc__isnull=False)
+    for item in u:
+        resp = os.system(f'ping -c 1 -w 1 {item.ipmi_bmc}')
+        if not resp:
+            item.ipmi_is_avaliable = True
+            item.save()
+        else:
+            item.ipmi_is_avaliable = False
+            item.save()
+
+if args:
+    if args[0] == 'loop':
+        while True:
+            main()
+            sleep(300)
     else:
-        item.is_avaliable = False
-        item.save()
-u = Units.objects.filter(ipmi_bmc__isnull=False)
-for item in u:
-    resp = os.system(f'ping -c 1 -w 1 {item.ipmi_bmc}')
-    if not resp:
-        item.ipmi_is_avaliable = True
-        item.save()
-    else:
-        item.ipmi_is_avaliable = False
-        item.save()
+        print("Error: unknown parameters")
+else:
+    main()
--- boot.sh
+++ boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 uwsgi --daemonize2 /tmp/nodes_uwsgi.log --need-app --ini nodes_uwsgi.ini
 /bin/bash
+
